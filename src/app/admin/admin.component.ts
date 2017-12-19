461f8c1cdff64109fd5b6234553b37cb922e2fde
Pierre Mehaye <pierre.753@hotmail.fr>
2017-12-19 14:28:28 +0100

    slight improvement of cart-detailled

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientDataService } from '../services/clientData.service';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Address } from '../models/address.model';
 
 @Component({
   selector: 'app-admin',
@@ -6,12 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
+  public addresses: Observable<Address[]>;
   private cmd= '';
 
-  constructor() { }
+  constructor(private clientDataService: ClientDataService,
+    private auth: AuthService,
+  private router: Router) { }
+
+    url: string = this.router.routerState.snapshot.url;
 
   ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.auth.ensureAuthenticated(token).then(user => {
+        console.log(user);
+        if (user.status === 'success') {
+          this.addresses = this.clientDataService.getAddress();
+        }else {
+          this.router.navigate(['/login'], { queryParams: { returnUrl: this.url }});
+        }
+      }).catch(err => console.log(err));
+    }else {
+      this.router.navigate(['/login'], { queryParams: { returnUrl: this.url }});
+    }
   }
 
   launchCmd() {
